5b4c978c9a4b1453ecc02dae89fa25643f7dc4ee
CodetrainGen14 <67614104+CodetrainGen14@users.noreply.github.com>
2020-06-30 14:54:35 +0000

    refactor UserItem

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-class UserList extends React.Component {
+class UserItem extends React.Component {
   // You can also do - "class UserList extends Component",
   // just that you'd have to import React this way -
   // import React, {Component} from "react".
+  constructor(props) {
+    super(props);
+  }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1></h1>
+        <h3></h3>
+      </div>
+    );
   }
 }
 
-export default UserList;
+export default UserItem;
