46a6761722f622c58dc4196d11e40c7589e026d8
Tony <toneyobaze@gmail.com>
2020-06-30 09:46:41 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* Form to add new user */}
+        <div>
+          <form>
+            <input type="text" placeholder="Name" />
+            <br />
+            <input type="email" placeholder="Email" />
+            <br />
+            <input type="submit" />
+          </form>
+        </div>
+
+        {/* List of users */}
+        <div>
+          <h3>List of users</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/UserList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserList extends React.Component {
+  // You can also do - "class UserList extends Component",
+  // just that you'd have to import React this way -
+  // import React, {Component} from "react".
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default UserList;
