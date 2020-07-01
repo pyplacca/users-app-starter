1c22d9b82c2c5d67d379efa2adf45158e4b6cf59
pyPlacca <dhegreatprosperity10@gmail.com>
2020-07-01 15:00:10 +0000

    final work commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React from "react";
 import "./App.css";
+import UserItem from "./components/UserItem"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      users: [],
+    }
+    // refs
+    this._name = React.createRef()
+    this._email = React.createRef()
+    // submit handler
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit (ev) {
+    ev.preventDefault()
+    // update users list
+    this.setState({
+      users: [...this.state.users, {
+        name:this._name.current.value, 
+        email:this._email.current.value
+      }]
+    })
+    // reset input fields
+    this._name.current.value = ''
+    this._email.current.value = ''
+    this._name.current.focus() // redirect focus to the name input
   }
 
   render() {
     return (
       <div className="App">
-        {/* Form to add new user */}
-        <div>
-          <form>
-            <input type="text" placeholder="Name" />
+        <div className="entry">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Name" ref={this._name}/>
             <br />
-            <input type="email" placeholder="Email" />
+            <input type="email" placeholder="Email" ref={this._email}/>
             <br />
             <input type="submit" />
           </form>
         </div>
 
-        {/* List of users */}
-        <div>
+        <div className="display">
           <h3>List of users</h3>
+          <div className="users">
+            {
+              this.state.users.map(
+                (user, i) => <UserItem {...user} key={i}/>
+              )
+            }
+          </div>
         </div>
       </div>
     );
--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,18 +1,13 @@
 import React from "react";
 
 class UserItem extends React.Component {
-  // You can also do - "class UserList extends Component",
-  // just that you'd have to import React this way -
-  // import React, {Component} from "react".
-  constructor(props) {
-    super(props);
-  }
 
   render() {
+    const {name, email} = this.props
     return (
-      <div>
-        <h1></h1>
-        <h3></h3>
+      <div className="user-item">
+        <h3 className="name">{name}</h3>
+        <h5 className="email">{email}</h5>
       </div>
     );
   }
